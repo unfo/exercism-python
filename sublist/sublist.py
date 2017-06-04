8f49fa7181a41f0c1b5d0d3bf12bd0567b285e07
Jan Wikholm <otp@localhost.exposed>
2017-06-04 21:08:23 +0300

    Semi-slow sublist implementation
    Working version at least.

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,2 +1,29 @@
-def check_lists():
-    pass
+SUBLIST = -1
+SUPERLIST = 9001
+EQUAL = 0
+UNEQUAL = 1
+
+
+def check_lists(a, b):
+    if a == b:
+        return EQUAL
+
+    if sublist(a, b):
+        return SUBLIST
+
+    if superlist(a, b):
+        return SUPERLIST
+
+    return UNEQUAL
+
+
+def sublist(a, b):
+    if len(a) > len(b):
+        return False
+    lena = len(a)
+    lenb = len(b)
+    return any(b[n:n+lena] == a for n in range(lenb))
+
+
+def superlist(a, b):
+    return sublist(b, a)
